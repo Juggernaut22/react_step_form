59ef202aba277ee954e307264b51ef9609109a08
Ramin Abdullayev <r.abdullayev@email.com>
2023-11-29 16:24:58 +0100

    4:25

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class FormUserDetails extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+            Hello from user details
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default FormUserDetails;
